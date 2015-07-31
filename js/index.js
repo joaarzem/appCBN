5655ec51a40661c737b6428d103e59c595fc710d
CBN <cbn@CBNs-MacBook-Pro.local>
2015-07-31 11:33:50 -0400

    login

--- js/index.js
+++ js/index.js
@@ -2,8 +2,34 @@
 
 jQuery(document).ready(function($) {
 	document.addEventListener("deviceready", ondeviceready, false);
+	$('#formulario').submit(function() { 
+
+
+	// recolecta los valores que inserto el usuario
+	var datosUsuario = $("#txt-number").val()
+	var datosPassword = $("#txt-password").val()
+	
+	archivoValidacion = "http://www.cbn.com.bo/webpedidos/cbnapp/validacion_de_datos.php?jsoncallback=?"
+	$.getJSON( archivoValidacion, { usuario:datosUsuario ,password:datosPassword})
+	.done(function(respuestaServer) {
+		
+		alert(respuestaServer.mensaje + "\nGenerado en: " + respuestaServer.hora + "\n" +respuestaServer.generador)
+		
+		if(respuestaServer.validacion == "ok"){
+
+		 	/// si la validacion es correcta, muestra la pantalla "home"
+		 	$.mobile.changePage("#home")
+
+		 }else{
+
+		  /// ejecutar una conducta cuando la validacion falla
+		}
+
+	})
+	return false;
+})
 });
 
 function ondeviceready() {
-    console.log("Entro aplicacion")
+	console.log("Entro aplicacion")
 }
